94af2a6f158b1173f2008a4dd2984bd0c37e4c53
lambertusjr <85484228+lambertusjr@users.noreply.github.com>
2026-01-27 11:05:21 +0200

    Submitting HM job

--- submit_RP.sh
+++ submit_RP.sh
@@ -60,7 +60,7 @@ python -c "import torch, sys; print('torch', torch.__version__, 'cuda', getattr(
 if [[ -f train.py ]]; then
   echo "Starting Worker 0 on GPU 0 (HiMedium)"
   # Run in background with & and redirect output
-  python -u train.py IBM_AML_LiMedium 
+  python -u train.py IBM_AML_HiMedium 
   
   # Wait for all background jobs to finish before cleanup
   wait
